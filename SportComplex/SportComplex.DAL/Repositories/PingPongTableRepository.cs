94dfb2e49e89244811245872aa481e7cc44f02b3
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-12-27 02:40:03 +0300

    add repositories

---
+++ SportComplex/SportComplex.DAL/Repositories/PingPongTableRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportComplex.DAL.Entities;
+using SportComplex.DAL.Interfaces;
+
+namespace SportComplex.DAL.Repositories
+{
+    public class PingPongTableRepository : GenericRepository<PingPongTableEntity>, IPingPongTableRepository
+    {
+        public PingPongTableRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ SportComplex/SportComplex.DAL/Repositories/ReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportComplex.DAL.Entities;
+using SportComplex.DAL.Interfaces;
+
+
+namespace SportComplex.DAL.Repositories
+{
+    public class ReviewRepository : GenericRepository<ReviewEntity>, IReviewRepository
+    {
+        public ReviewRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
