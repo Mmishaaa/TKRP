641d928af0d24d13906e46e021f052ea37b55f58
MatsRodionDev <162904839+MatsRodionDev@users.noreply.github.com>
2024-12-27 04:06:30 +0300

    create services

---
+++ SportComplex/SportComplex.BLL/Services/SwimmingBookingService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SportComplex.BLL.Interfaces;
+using SportComplex.BLL.Models;
+using SportComplex.DAL.Entities;
+using SportComplex.DAL.Interfaces;
+
+namespace SportComplex.BLL.Services
+{
+    public class SwimmingBookingService : GenericService<SwimmingPoolBookingModel, SwimmingPoolBookingEntity>, ISwimmingPoolBookingService
+    {
+        public SwimmingBookingService(ISwimmingPoolBookingRepository repository, IMapper mapper)
+            : base(repository, mapper)
+        {
+        }
+    }
+}
---
+++ SportComplex/SportComplex.BLL/Services/SwimmingPoolService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SportComplex.BLL.Interfaces;
+using SportComplex.BLL.Models;
+using SportComplex.DAL.Entities;
+using SportComplex.DAL.Interfaces;
+
+namespace SportComplex.BLL.Services
+{
+    public class SwimmingPoolService : GenericService<SwimmingPoolModel, SwimmingPoolEntity>, ISwimmingPoolService
+    {
+        public SwimmingPoolService(ISwimmingPoolRepository repository, IMapper mapper)
+            : base(repository, mapper)
+        {
+        }
+    }
+}
