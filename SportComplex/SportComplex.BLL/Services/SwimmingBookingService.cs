1ecd5c1976057a7cc9acac5527aa50e2f56fc3d6
Mmishaaa <138068630+Mmishaaa@users.noreply.github.com>
2024-12-27 04:13:55 +0300

    add last services

--- SportComplex/SportComplex.BLL/Services/SwimmingBookingService.cs
+++ SportComplex/SportComplex.BLL/Services/SwimmingBookingService.cs
@@ -6,9 +6,9 @@ using SportComplex.DAL.Interfaces;
 
 namespace SportComplex.BLL.Services
 {
-    public class SwimmingBookingService : GenericService<SwimmingPoolBookingModel, SwimmingPoolBookingEntity>, ISwimmingPoolBookingService
+    public class SwimmingPoolBookingService : GenericService<SwimmingPoolBookingModel, SwimmingPoolBookingEntity>, ISwimmingPoolBookingService
     {
-        public SwimmingBookingService(ISwimmingPoolBookingRepository repository, IMapper mapper)
+        public SwimmingPoolBookingService(ISwimmingPoolBookingRepository repository, IMapper mapper)
             : base(repository, mapper)
         {
         }
